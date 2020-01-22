e2ca3a9f20916848ddaf0d984975f2e7d827d3ad
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-01-22 21:38:58 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,7 +13,7 @@ class StudentsController < ApplicationController
 
   def create
     Student.create(first_name: params[:post][:first_name], last_name: params[:post][:last_name])
-    redirect_to posts_path
+    redirect_to students_path
   end
 
 end
